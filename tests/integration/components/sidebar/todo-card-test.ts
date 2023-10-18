5b6b2fc478490871294512c87304c81c12ca214d
Abraham Lopez <abraham.lopdel@gmail.com>
2023-10-18 09:42:41 +0200

    Test todo-card 02 with some errors

--- tests/integration/components/sidebar/todo-card-test.ts
+++ tests/integration/components/sidebar/todo-card-test.ts
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { click, render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | sidebar/todo-card', function (hooks) {
@@ -27,5 +27,23 @@ module('Integration | Component | sidebar/todo-card', function (hooks) {
       .hasAttribute('src', '/delete-24.svg', 'Icon has the apropriate image');
 
     assert.dom('[data-test-card-wrapper-link] hr').exists();
+  });
+
+  test(`02 - GIVEN the todo card component
+  WHEN button with delete icon is clicked
+  THEN it deletes the current todo`, async function (assert) {
+    this.setProperties({
+      todo: {
+        id: '3000',
+        todoTitle: 'New Todo',
+      },
+    });
+
+    await render(hbs`<Sidebar::TodoCard @todo={{this.todo}} />`);
+
+    assert.dom('[data-test-delete-button]').exists('Delete button exists');
+
+    await click('[data-test-delete-button]');
+    assert.dom('[data-test-card-wrapper-link] > div').doesNotExist();
   });
 });
